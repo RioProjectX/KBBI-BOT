bfddd4deaf1fabd64e21cf58707c70bf3d3cf7ff
Barklei <81870902+nekozu@users.noreply.github.com>
2021-07-27 15:29:37 +0700

    halooo sayng muachh

---
+++ main.js
@@ -0,0 +1,23 @@
+const { Bot, InlineKeyboard } = require('grammy')
+const kbbi = require('kbbi-scraper');
+
+const bot = new Bot(process.env.token)
+
+bot.command('start', (ctx) => ctx.reply(process.env.start_text))
+
+bot.command('kata', (ctx) => {
+    let input = ctx.message.text;
+    let inputArray = input.split(" ");
+    inputArray.shift();
+    const query = inputArray.join(" "); 
+    const keyboard = new InlineKeyboard().url('Cari diweb kbbi', 'https://kbbi.kemdikbud.go.id/entri/'+query)                                                                                                                                                                
+    kbbi(query).then(res => {
+     ctx.reply(res.data.arti, {
+        reply_markup: keyboard
+       })
+    }).catch(e => {
+         ctx.reply(e);
+   })
+})
+
+bot.start();
