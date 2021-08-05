ee77fc53afd06dd6721cedb5a51f334f018a97de
Barklei <81870902+nekozu@users.noreply.github.com>
2021-08-05 19:42:36 +0700

    bug

--- index.js
+++ index.js
@@ -18,27 +18,7 @@ bot.command('kata', (ctx) => {
     }).catch(e => {
          ctx.reply(e);
    })
-})
-bot.on('inline_query', (ctx) => {
-    let all_data = ctx.inlineQuery
-    let query = ctx.inlineQuery.query
-    let k = kbbi(query)
-        result = [
-            {
-                type: "article",
-                id: 1,
-                title: "KBBI",
-                description: "Ditemukan",
-                input_message_content: {
-                    message_text: k.data.arti,
-                    parse_mode: "HTML"
-            },
-      reply_markup: Markup.inlineKeyboard([
-        Markup.button.url('Lihat Di Web', 'https://kbbi.kemdikbud.go.id/entri/'+query)
-      ])
-    })
-  return await ctx.answerInlineQuery(recipes)
-})
+})          
 
 console.log('Bot Mu sudah berjalan dengan baik')
 console.log('Jangan lupa subs @nekozu ya!')
